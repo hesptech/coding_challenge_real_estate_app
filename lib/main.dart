68121c3c445cb96d0c37558812fa37ad58372955
hesptech <olea.oswaldo@gmail.com>
2024-01-15 09:18:42 +0100

    feat: init riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'package:coding_challenge_real_estate_app/config/config.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const ProviderScope(child: MainApp()));
 }
 
 class MainApp extends StatelessWidget {
